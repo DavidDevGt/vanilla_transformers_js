e3410a6dac0047191dbfc6ff11b737902040aa7b
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-10 15:34:58 -0600

    Update object detection functionality and remove progress bar

--- index.js
+++ index.js
@@ -8,33 +8,30 @@ env.allowLocalModels = false;
 const fileUpload = document.getElementById("file-upload");
 const clearSelectionButton = document.getElementById("clear-selection");
 const imageContainer = document.getElementById("image-container");
-const progressBar = document.getElementById("progress-bar");
 const status = document.getElementById("status");
 
 let detector;
 
 async function loadModel() {
-  status.textContent = "Cargando modelo de detección de objetos...";
+  updateStatus("Cargando modelo de detección de objetos...");
   detector = await pipeline("object-detection", "Xenova/detr-resnet-50");
-  status.textContent = "Modelo cargado. ¡Sube una imagen para empezar!";
+  updateStatus("Modelo cargado. ¡Sube una imagen para empezar!");
 }
 
-function updateProgressBar(progress) {
-  progressBar.style.width = `${progress}%`;
+function updateStatus(message) {
+  status.textContent = message;
 }
 
 function resetApplication() {
   fileUpload.value = "";
   imageContainer.innerHTML = "";
-  status.textContent = "Modelo cargado. ¡Sube una imagen para empezar!";
-  updateProgressBar(0);
+  updateStatus("Modelo cargado. ¡Sube una imagen para empezar!");
 }
 
 fileUpload.addEventListener("change", async function (e) {
   const file = e.target.files[0];
   if (!file) {
-    status.textContent = "No se seleccionó ninguna imagen.";
-    updateProgressBar(0);
+    updateStatus("No se seleccionó ninguna imagen.");
     return;
   }
   readAndDetectImage(file);
@@ -43,9 +40,7 @@ fileUpload.addEventListener("change", async function (e) {
 clearSelectionButton.addEventListener("click", resetApplication);
 
 async function readAndDetectImage(file) {
-  status.textContent = "Cargando imagen...";
-  updateProgressBar(10); // pequeño progreso por la carga de la imagen
-
+  updateStatus("Cargando imagen...");
   const reader = new FileReader();
   reader.onload = async function (e) {
     displayImage(e.target.result);
@@ -59,23 +54,19 @@ function displayImage(imageSrc) {
   const image = new Image();
   image.src = imageSrc;
   imageContainer.appendChild(image);
-  updateProgressBar(30); // Progreso por haber cargado la imagen en el contenedor
+  updateStatus("Imagen cargada. Analizando...");
 }
 
 async function detectObjects(imageSrc) {
-  status.textContent = "Analizando imagen...";
-  updateProgressBar(50); // Progreso inicial del análisis
-
   const output = await detector(imageSrc, { threshold: 0.5, percentage: true });
-  updateProgressBar(100); // Completar la barra al terminar el análisis
-
   if (output.length === 0) {
-    status.textContent = "No se detectaron objetos en la imagen.";
+    updateStatus("No se detectaron objetos en la imagen.");
   } else {
-    status.textContent = `Se detectaron ${output.length} objetos.`;
+    updateStatus(`Se detectaron ${output.length} objetos.`);
     output.forEach(renderBox);
   }
 }
+
 function renderBox({ box, label }) {
   const { xmax, xmin, ymax, ymin } = box;
   const color =
