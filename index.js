19603e270a844c690bf8a84c8244348c9fbc57f9
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-10 14:28:13 -0600

    Add object detection functionality to image uploader

--- index.js
+++ index.js
@@ -8,13 +8,69 @@ const fileUpload = document.getElementById("file-upload");
 const imageContainer = document.getElementById("image-container");
 const status = document.getElementById("status");
 
-// Mensaje de espera
+// Mensaje de espera, luego llamar a la pipeline
 status.textContent = "Cargando modelo...";
-
-// Llamar a la pipeline
 const detector = await pipeline("object-detection", "Xenova/detr-resnet-50");
 
 // Cuando retorne el modelo, cambiar el mensaje
 status.textContent = "¡Listo para detectar objetos!";
 
 // Crear la logica para cargar la imagen
+fileUpload.addEventListener("change", function (e) {
+    const file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+
+    const reader = new FileReader();
+
+    // Configurar callback cuando el archivo se haya subido
+    reader.onload = function (e2) {
+        imageContainer.innerHTML = "";
+        const image = document.createElement("img");
+        image.src = e2.target.result;
+        imageContainer.appendChild(image);
+        detect(image);
+        // Lo de arriba si se descomenta corre el modelo
+    };
+    reader.readAsDataURL(file);
+});
+
+// Con el modelo cargado, detectar objetos
+async function detect(img) {
+    status.textContent = 'Analysing...';
+    const output = await detector(img.src, {
+        threshold: 0.5,
+        percentage: true,
+    });
+    status.textContent = '';
+    output.forEach(renderBox);
+}
+
+// Esto genera el cuadro de la deteccion
+function renderBox({ box, label }) {
+    const { xmax, xmin, ymax, ymin } = box;
+
+    // Generate a random color for the box
+    const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, 0);
+
+    // Draw the box
+    const boxElement = document.createElement('div');
+    boxElement.className = 'bounding-box';
+    Object.assign(boxElement.style, {
+        borderColor: color,
+        left: 100 * xmin + '%',
+        top: 100 * ymin + '%',
+        width: 100 * (xmax - xmin) + '%',
+        height: 100 * (ymax - ymin) + '%',
+    });
+
+    // Generar el label para el cuadro
+    const labelElement = document.createElement('span');
+    labelElement.textContent = label;
+    labelElement.className = 'bounding-box-label';
+    labelElement.style.backgroundColor = color;
+
+    boxElement.appendChild(labelElement);
+    imageContainer.appendChild(boxElement);
+}
